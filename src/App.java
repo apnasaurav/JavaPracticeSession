ef5cce711723ca7006e5ce8ae898e829c9b4a5bd
Saurav <saurav30798@gmail.com>
2023-02-24 16:41:11 +0530

    24 Feb

--- src/App.java
+++ src/App.java
@@ -1,5 +1,26 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int i =0;
+        int j =0;
+        int n=10;
+        int s;
+        for(i=1;i<n;i++)
+        {
+            for(j=1;j<n;j++)
+            {
+                s=i+j;
+                if((i==j || s==n) && i<=n/2)
+                {
+                    System.out.println(n-i);
+                }
+                else
+                {
+                    System.out.println(" ");
+                }
+            }
+            System.out.println();
+        }
     }
+    
+    
 }
